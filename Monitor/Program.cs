614246f61c7456afdb845dd09747f748383c13c1
DemDemich <41775352+DemDemich@users.noreply.github.com>
2019-02-17 17:52:00 +0500

    Initial Commit

---
+++ Monitor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
