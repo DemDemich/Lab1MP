dec32fd32ce407ea88af70a2c1717b6258dfb8ef
Mite0 <43238130+Mite0@users.noreply.github.com>
2019-02-21 18:23:02 +0500

    q

--- Monitor/Monitor/Program.cs
+++ Monitor/Monitor/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.IO.MemoryMappedFiles;
+using System.IO;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Linq;
@@ -12,8 +13,33 @@ namespace Monitor
 {
     class Program
     {
+        private const UInt32 StdOutputHandle = 0xFFFFFFF5;
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetStdHandle(UInt32 nStdHandle);
+        [DllImport("kernel32.dll")]
+        private static extern void SetStdHandle(UInt32 nStdHandle, IntPtr handle);
+        [DllImport("kernel32")]
+        static extern bool AllocConsole();
+        public static void CreateConsole()
+        {
+            AllocConsole();
+
+            // stdout's handle seems to always be equal to 7
+            IntPtr defaultStdout = new IntPtr(7);
+            IntPtr currentStdout = GetStdHandle(StdOutputHandle);
+
+            if (currentStdout != defaultStdout)
+                // reset stdout
+                SetStdHandle(StdOutputHandle, defaultStdout);
+
+            // reopen stdout
+            TextWriter writer = new StreamWriter(Console.OpenStandardOutput())
+            { AutoFlush = true };
+            Console.SetOut(writer);
+        }
         static void Main(string[] args)
         {
+            CreateConsole();
             int hCount = int.Parse(args[0]);//int.Parse(args[0]);
             Console.Title = $"Длинна поля:{args[1]}";
             ProgressBarCreator pg = new ProgressBarCreator(hCount); //создание кол-ва прогрессбаров
